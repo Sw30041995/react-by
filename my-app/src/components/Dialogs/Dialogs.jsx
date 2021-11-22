9aaf931c16bb283afa6024da45290331a7c42f8b
sw30041995 <sw1225@yandex.ru>
2021-11-22 23:19:47 +0300

    new file

--- my-app/src/components/Dialogs/Dialogs.jsx
+++ my-app/src/components/Dialogs/Dialogs.jsx
@@ -4,38 +4,32 @@ import DialogItem from "./DialogItem/DialogItem";
 
 const Dialogs = (props) => {
     let dialogsData = [
-        { id: 1, name: 'Ivan'},
-        { id: 2, name: 'Yaroslav'},
-        { id: 3, name: 'Alexey'},
-        { id: 4, name: 'Sergey'},
-        { id: 5, name: 'Evgeniy'}
+        {id: 1, name: 'Ivan'},
+        {id: 2, name: 'Yaroslav'},
+        {id: 3, name: 'Alexey'},
+        {id: 4, name: 'Sergey'},
+        {id: 5, name: 'Evgeniy'}
+    ];
+    let messagesData = [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'Yo'},
+        {id: 3, message: 'Hello my friend'},
+        {id: 4, message: 'heeeey'},
+        {id: 5, message: 'I learn React and JS'},
     ];
 
-    let messagesData = [
-        { id: 1, message: 'Hi' },
-        { id: 2, message: 'Yo' },
-        { id: 3, message: 'Hello my friend' },
-        { id: 4, message: 'heeeey' },
-        { id: 5, message: 'I learn React and JS' },
-    ];
+    let dialogsElements = dialogsData.map(dialog => <DialogItem name={dialogsData.name} id={dialogsData.id} />);
+    let messagesElements = messagesData.map(message => <Message message={messagesData.message} />);
 
     return (
-      <div className={style.dialogs}>
-          <div className={style.dialogsItems}>
-              <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
-              <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
-              <DialogItem name={dialogsData[2].name} id={dialogsData[2].id} />
-              <DialogItem name={dialogsData[3].name} id={dialogsData[3].id} />
-              <DialogItem name={dialogsData[4].name} id={dialogsData[4].id} />
-          </div>
-          <div className={style.messages}>
-              <Message message={messagesData[0].message} />
-              <Message message={messagesData[1].message} />
-              <Message message={messagesData[2].message} />
-              <Message message={messagesData[3].message} />
-              <Message message={messagesData[4].message} />
-          </div>
-      </div>
+        <div className={style.dialogs}>
+            <div className={style.dialogsItems}>
+                {dialogsElements}
+            </div>
+            <div className={style.messages}>
+                {messagesElements}
+            </div>
+        </div>
     );
 }
 
