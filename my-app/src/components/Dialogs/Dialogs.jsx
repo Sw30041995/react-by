b59dbb4d5ad44cceda41f0b4f10e1ccae53ba163
sw30041995 <sw1225@yandex.ru>
2021-11-22 22:21:16 +0300

    new file

--- my-app/src/components/Dialogs/Dialogs.jsx
+++ my-app/src/components/Dialogs/Dialogs.jsx
@@ -3,6 +3,14 @@ import Message from "./Message/Message";
 import DialogItem from "./DialogItem/DialogItem";
 
 const Dialogs = (props) => {
+    let dialogsData = [
+        { id: 1, name: 'Ivan'},
+        { id: 2, name: 'Yaroslav'},
+        { id: 3, name: 'Alexey'},
+        { id: 4, name: 'Sergey'},
+        { id: 5, name: 'Evgeniy'},
+    ];
+
     return (
       <div className={style.dialogs}>
           <div className={style.dialogsItems}>
@@ -10,7 +18,7 @@ const Dialogs = (props) => {
               <DialogItem name='Yaroslav' id='2' />
               <DialogItem name='Alexey' id='3' />
               <DialogItem name='Sergey' id='4' />
-              <DialogItem name='Ivan' id='5' />
+              <DialogItem name='Evgeniy' id='5' />
           </div>
           <div className={style.messages}>
               <Message message='Hi' />
