a30ca6f7b02c851a588eb2d8efd040abbd73ed8d
sw30041995 <sw1225@yandex.ru>
2021-11-22 22:34:52 +0300

    new file

--- my-app/src/components/Profile/MyPosts/MyPosts.jsx
+++ my-app/src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,7 +2,7 @@ import style from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = () => {
-    let messagesData = [
+    let postsData = [
         { id: 1, message: 'Hello', likesCount: 15 },
         { id: 2, message: 'How are you', likesCount: 7 }
     ];
@@ -18,8 +18,8 @@ const MyPosts = () => {
                     <button>Post</button>
                 </div>
             </div>
-                <Post message='Hello' likesCount='15'/>
-                <Post message='How are you?' likesCount='7'/>
+                <Post message={postsData[0].message} likesCount={postsData[0].likesCount} />
+                <Post message={postsData[1].message} likesCount={postsData[1].likesCount} />
         </div>
     );
 }
