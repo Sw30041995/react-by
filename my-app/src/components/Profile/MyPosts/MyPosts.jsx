d03acc096c2f270b817b6e8dd9574750aa5ef713
sw30041995 <sw1225@yandex.ru>
2021-11-23 21:27:06 +0300

    new file

--- my-app/src/components/Profile/MyPosts/MyPosts.jsx
+++ my-app/src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,12 +1,7 @@
 import style from './MyPosts.module.css';
 import Post from './Post/Post';
 
-const MyPosts = () => {
-    let postsData = [
-        { id: 1, message: 'Hello', likesCount: 15 },
-        { id: 2, message: 'How are you', likesCount: 7 }
-    ];
-
+const MyPosts = (props) => {
     return (
         <div>
             <h3>My posts</h3>
@@ -18,8 +13,7 @@ const MyPosts = () => {
                     <button>Post</button>
                 </div>
             </div>
-                <Post message={postsData[0].message} likesCount={postsData[0].likesCount} />
-                <Post message={postsData[1].message} likesCount={postsData[1].likesCount} />
+            {postsElement}
         </div>
     );
 }
--- my-app/src/components/Profile/Profile.jsx
+++ my-app/src/components/Profile/Profile.jsx
@@ -1,8 +1,15 @@
 import style from './Profile.module.css';
 import MyPosts from './MyPosts/MyPosts';
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
+import Post from "./MyPosts/Post/Post";
 
-const Profile = () => {
+let postsData = [
+    {id: 1, message: 'Hello', likesCount: 15},
+    {id: 2, message: 'How are you', likesCount: 7}
+];
+let postsElement = postsData.map(post => <Post message={post.message} likesCount={post.likesCount} />);
+
+const Profile = (props) => {
     return (
         <div className={style.content}>
             <ProfileInfo />
